c44f6d333b44537e9139088ffc889fe5e556d732
Emir <gsgtrascal@live.com>
2021-11-13 14:07:48 +0300

    Add project files.

---
+++ ProblemIki/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*TODO
+  Belli bir formata göre hazırlanmış aşağıdaki stringden standard outputta aşağıdaki çıktıyı üreten bir kod yazılmasını bekliyoruz.
+
+  Varsayımlar ve kurallar:
+  •Verilen stringte her harf 2 defa kullanılır ve aynı 2 harf arasında kalan harfler child harflerdir.
+  •Verilen stringin daima düzgün formatta verildiğini kabul ediyoruz. String formatını kontrol edilmesine gerek yok.
+  •Standart outputta ağaç yapısını gösterecek şekilde bir çıktı bekliyoruz.
+  •Verilen string sonsuz derinlikte bir ağaç olabilir.
+
+  Örnek 1:  Input:  abccbdeeda
+            Output:
+                    a
+                    -b
+                    --c
+                    -d
+                    --e
+
+  Örnek 2:  Input:  abccddbeea
+            Output:
+                    a
+                    -b
+                    --c
+                    --d
+                    -e
+*/
+
+namespace ProblemIki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
